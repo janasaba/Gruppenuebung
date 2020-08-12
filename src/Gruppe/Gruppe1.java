48f333321c197aecbba6e9e5be617cff598e72b5
Rainer Classen <rainer.classen@awacademyconsultant.de>
2020-08-12 12:41:48 +0200

    Test schwieriger gemacht!

--- src/Gruppe/Gruppe1.java
+++ src/Gruppe/Gruppe1.java
@@ -5,29 +5,20 @@ import java.util.Scanner;
 public class Gruppe1 {
 
 	public static void main(String[] args) {
-
-		
 		
 		System.out.println("Wie viele Bundesländer hat Deutschland?");
-		System.out.println("16 oder 3 oder 15 oder 30?");
+		//System.out.println("16 oder 3 oder 15 oder 30?");
 
 		Scanner scan = new Scanner(System.in);
 		int bLand = scan.nextInt();
-
 		if (bLand == 16) {
 			System.out.println("Richtig!");
-
-		} else if (bLand == 3) {
+		} else if (bLand < 16) {
 			System.out.println("Leider falsch");
-		} else if (bLand == 6) {
+		} else if (bLand > 16) {
 			System.out.println("so viele Bundeslaender haben wir nicht");
-		} else if (bLand == 15)
-			;
-		{
-			System.out.println("Ist richtig");
-			
+					
 			scan.close();
-
 		}
 	}
 
